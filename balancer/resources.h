2f7c9bd89765b7485f4fd2411451827fbc8cf984
Bryan Call <bcall@apache.org>
2011-11-14 23:09:44 +0000

    Added plugin to load balance traffic.  Code from Yahoo!.
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/plugins/trunk@1201960 13f79535-47bb-0310-9956-ffa450edef68

---
+++ balancer/resources.h
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Implement the resource class (per request), for optimal processing speed.
+//
+#ifndef __RESOURCES_H__
+#define __RESOURCES_H__ 1
+
+
+#include <RemapAPI.h>
+#include <InkAPI.h>
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Class declaration
+//
+class Resources
+{
+public:
+  Resources(INKHttpTxn txnp, TSRemapRequestInfo *rri) :
+    _txnp(txnp), _rri(rri), _jar(NULL), _bufp(NULL), _hdrLoc(NULL)
+  { }
+
+  ~Resources() {
+    if (_hdrLoc) {
+      INKDebug("balancer", "Releasing the client request headers");
+      INKHandleMLocRelease(_bufp, INK_NULL_MLOC, _hdrLoc);
+    }
+
+    if (_jar) {
+      INKDebug("balancer", "Destroying the cookie jar");
+      // TODO - destroy cookies
+    }
+  }
+
+  const INKHttpTxn getTxnp() const { return _txnp; }
+    
+  const TSRemapRequestInfo* getRRI() const { return _rri; }
+
+  const cookiejar_t
+  getJar() {
+    if (_jar)
+      return _jar;
+
+    // Setup the cookie jar for all processing
+    if (_rri->request_cookie_size > 0) {
+      char cookie_hdr[_rri->request_cookie_size + 1];
+
+      memcpy(cookie_hdr, _rri->request_cookie, _rri->request_cookie_size);
+      cookie_hdr[_rri->request_cookie_size] = '\0';
+      _jar = // TODO - create cookies
+      INKDebug("balancer", "Creating the cookie jar");
+    }
+
+    return _jar;
+  }
+
+  const INKMBuffer
+  getBufp() {
+    if (_bufp) {
+      return _bufp;
+    } else {
+      if (!_txnp || !INKHttpTxnClientReqGet(_txnp, &_bufp, &_hdrLoc)) {
+        _bufp = NULL;
+        _hdrLoc = NULL;
+      }
+      return _bufp;
+    }
+  }
+
+  const INKMLoc
+  getHdrLoc() {
+    if (!_bufp || !_hdrLoc)
+      (void)getBufp();
+
+    return _hdrLoc;
+  }
+
+private:
+  INKHttpTxn _txnp;
+  TSRemapRequestInfo* _rri;
+  cookiejar_t _jar;
+  INKMBuffer _bufp;
+  INKMLoc _hdrLoc;
+};
+
+
+#endif // __HASHKEY_H
